0176f724e020304ded6c4b50eaa45dd83c3d555c
Julien Tesson <julien.tesson@nomadic-labs.com>
2020-11-19 00:42:02 +0100

    ppx-debug: Replace Lwt with Lwt_debug

--- src/ppx-debug/main.ml
+++ src/ppx-debug/main.ml
@@ -60,22 +60,53 @@ let rewriter exp (node : Parsetree.expression) =
         (List.assoc_opt (Labelled "pos") args)
   | _ -> None
 
-(**declaration of the extension*)
-let exp_of_fun_id loc id =
-  let ident_of_str str =
-    let split = String.split_on_char '.' str in
-    match split with
-    | [] -> failwith ("invalid_arg" ^ __LOC__)
-    | [id] -> Lident id
-    | h :: next :: t ->
-        List.fold_left
-          (fun ident next -> Ldot (ident, next))
-          (Ldot (Lident h, next))
-          t
-  in
 
+let try_remove_prefix ~prefix lst =
+  let rec aux p l =
+    match p,l with
+      [] , _ -> (true,l)
+    | h::p , h'::l when h = h' -> aux p l
+    | _ -> (false,lst)
+  in aux prefix lst
+
+let rec pp_longident ppf (id:longident) =
+  match id with
+  | Ppxlib.Lident s -> Format.fprintf ppf "%s" s
+  | Ppxlib.Ldot (id, s) -> Format.fprintf ppf "%a.%s" pp_longident id s
+  | Ppxlib.Lapply (id, id') ->
+     Format.fprintf ppf
+       "%a.%a"
+       pp_longident id
+       pp_longident id'
+(**declaration of the extension*)
+let exp_of_fun_id ?replace_prefix ?replacement_prefix id =
+  let rep = Option.map (String.split_on_char '.') replace_prefix in
+  let split = String.split_on_char '.' id in
+  (* Format.printf
+   *   "replace_prefix:%s and add prefix %s in :%s@."
+   *   (Option.fold ~none:"" ~some:(String.concat ".") rep)
+   *   (Option.value ~default:"" prefix)
+   *   (String.concat "." split)
+   * ; *)
+  let removed_prefix,split = Option.fold
+                  ~none:(false,split)
+                  ~some:(fun prefix ->
+                    try_remove_prefix ~prefix split)
+                  rep in
+  let ident_of_id =
+    Longident.parse
+      (if removed_prefix then
+        Option.value ~default:"" replacement_prefix ^ String.concat "." (split)
+      else id) in
+  let txt  = ident_of_id in
+  (* Format.printf
+   *   "%s will be replaced by %a@."
+   *   id
+   *   pp_longident txt
+   * ; *)
+  fun loc ->
   {
-    pexp_desc = Pexp_ident { txt = ident_of_str id; loc };
+    pexp_desc = Pexp_ident { txt  ; loc };
     pexp_loc = loc;
     pexp_attributes = [];
     pexp_loc_stack = [];
@@ -87,9 +118,12 @@ let exp_of_fun_id loc id =
  *     Ast_pattern.(single_expr_payload (__))
  *     instr *)
 
+let replacement_prefix = "Lwt_debug."
+let replace_prefix = Some "Lwt"
+
 let rules =
   List.map (fun id ->
-      let rewriter = rewriter (fun loc -> exp_of_fun_id loc id) in
+      let rewriter = rewriter (exp_of_fun_id id ?replace_prefix ~replacement_prefix) in
       Context_free.Rule.special_function id rewriter)
   @@
   let positionnable =
